4cf0e87c67ee105b90d726d5b013bd1bab63d591
diamondburned <datutbrus@gmail.com>
2020-09-18 20:05:26 -0700

    Added scratch SQL

--- backend/go.mod
+++ backend/go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/dghubble/gologin v2.1.0+incompatible // indirect
 	github.com/go-chi/chi v4.1.2+incompatible // indirect
 	github.com/go-chi/httprate v0.4.0 // indirect
-	github.com/kyleconroy/sqlc v1.5.0 // indirect
+	github.com/vegaj/JOSE v0.0.0-20181009111100-d2b99d80d02d // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import (
+	"database/sql"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+func init() {
+	d, err := filepath.Glob("env*")
+	if err != nil {
+		log.Fatalln("Failed to get env* files:", err)
+	}
+
+	if len(d) == 0 {
+		log.Fatalln("No env files found.")
+	}
+
+	for _, f := range d {
+		if err := godotenv.Load(f); err != nil {
+			log.Fatalf("Failed to load %q: %v\n", f, err)
+		}
+	}
+}
+
+func main() {
+	d, err := sql.Open("pgx", os.Getenv("SQL_ADDRESS"))
+	if err != nil {
+		log.Fatalln("Failed to connect to PostgreSQL:", err)
+	}
+}
